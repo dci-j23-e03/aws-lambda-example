93a8bea2ea440ce7e4d1e3d88ee365f5b6597bce
dzenang <dzenan.granulo@gmail.com>
2024-11-04 15:51:08 +0100

    Add lambda function to log uploaded file details

---
+++ src/main/java/com/dzenang/S3Handler.java
@@ -0,0 +1,30 @@
+package com.dzenang;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class S3Handler implements RequestHandler<S3Event, String> {
+
+    @Override
+    public String handleRequest(S3Event s3Event, Context context) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        LambdaLogger logger = context.getLogger();
+
+        S3EventNotification.S3EventNotificationRecord record = s3Event.getRecords().getFirst();
+        String bucket = record.getS3().getBucket().getName();
+        String key = record.getS3().getObject().getUrlDecodedKey();
+
+        logger.log("Event:" + gson.toJson(s3Event));
+        logger.log("Context:" + gson.toJson(context));
+        logger.log("Record:" + gson.toJson(record));
+        logger.log("Bucket: " + bucket);
+        logger.log("Key: " + key);
+
+        return "File from S3 arrived!";
+    }
+}
